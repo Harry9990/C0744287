c41a471e37b6a6eb4277891cc9f860edabdf3776
visualstudio <visualstudio@230-PC42>
2019-02-07 11:49:15 -0500

    for loop committ

--- C0744287/js/main.js
+++ C0744287/js/main.js
@@ -6,4 +6,9 @@ function arrays() {
     var pens;
     pens = ["red", "orange", "green", "blue", "purple"];
     console.log(pens);
+    console.log(pens[0]);
+    for (i = 0; i < pens.length; i++) {
+        alert(pens[i]);
+    }
+    alert("Length of arrays is :", i);
 }
